474b497eb8db6e19dccea8a0d1f5f94a30f7291a
LoeweGuckmal <loegue123123@gmail.com>
2023-05-22 19:30:55 +0200

    update

--- script.js
+++ script.js
@@ -191,7 +191,8 @@ document.querySelector('#btn1').onclick = function(){
     if (toggled) {
         //alert("haha");
         fullscreen.request(this.parentNode);
-        window.screen.orientation.lock('portrait');
+        //window.screen.orientation.lock();
+        ScreenOrientation.lock("landscape");
         console.log("lock");
     }else {
         colorWindow = window.open("http://192.168.100.168:8880/color?color=bg");
